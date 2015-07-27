4c172d0d20d71be6dc036a37d28e95357ccd22b8
webforce3 <denis.nerborac@wf3.fr>
2015-07-27 21:59:46 +0200

    Update default controller + default route

--- config/routes.conf.php
+++ config/routes.conf.php
@@ -5,29 +5,7 @@ global $routes;
 $routes = array(
 	/* Front */
 	'/' => array(
-		'target' => 'home',
-		'action' => 'index'
-	),
-	'post/([0-9]+)' => array(
-		'target' => 'post',
-		'action' => 'view'
-	),
-	'post/archives/([0-9\-]+)/([0-9]+)' => array(
-		'target' => 'post',
-		'action' => 'archives'
-	),
-	'search' => array(
-		'target' => 'search',
-		'action' => 'results'
-	),
-
-	/* Backoffice */
-	'admin/contact/edit/([0-9\-]+)' => array(
-		'target' => 'admin',
-		'action' => 'contact_edit'
-	),
-	'admin/contact/delete/([0-9\-]+)' => array(
-		'target' => 'admin',
-		'action' => 'contact_delete'
+		'target' => DEFAULT_CONTROLLER_TARGET,
+		'action' => DEFAULT_CONTROLLER_ACTION
 	)
 );
